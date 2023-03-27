3df6d8048e53ff452d8453fc931b7dacb912ffd8
Евгений <eugene.lysakov@gmail.com>
2023-03-27 12:55:24 +0500

    5.2.2. Реализовать Store<T extends Base> [#157]

--- src/test/java/ru/job4j/generic/RoleStoreTest.java
+++ src/test/java/ru/job4j/generic/RoleStoreTest.java
@@ -7,7 +7,85 @@ import static org.assertj.core.api.Assertions.*;
 class RoleStoreTest {
 
     @Test
-    void whenAddThenRoleIsSoloist() {
+    void whenAddThenTypeIsCorrect() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Soloist"));
+        Role result = store.findById("1");
+        assertThat(result.getType()).isEqualTo("Soloist");
+    }
+
+    @Test
+    void whenAddThenFindByAnotherIDTisNull() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Soloist"));
+        Role result = store.findById("3");
+        assertThat(result).isNull();
+    }
+
+    @Test
+    void whenAddPresentThenSameType() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Soloist"));
+        store.add(new Role("1", "Assistant"));
+        Role result = store.findById("1");
+        assertThat(result.getType()).isEqualTo("Soloist");
+    }
+
+    @Test
+    void whenCorrectIDReplaceThenNewType() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Soloist"));
+        store.replace("1", new Role("1", "Assistant"));
+        Role result = store.findById("1");
+        assertThat(result.getType()).isEqualTo("Assistant");
+    }
+
+    @Test
+    void whenIncorrectIDReplaceThenTypeNotChanged() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Soloist"));
+        store.replace("10", new Role("10", "Assistant"));
+        Role result = store.findById("1");
+        assertThat(result.getType()).isEqualTo("Soloist");
+    }
+
+    @Test
+    void whenCorrectDeleteThenFindByIDTsNull() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Soloist"));
+        store.delete("1");
+        Role result = store.findById("1");
+        assertThat(result).isNull();
+    }
+
+    @Test
+    void whenDeleteAnotherThenPreviouslyAddedExists() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("3", "Prompter"));
+        store.delete("1");
+        Role result = store.findById("3");
+        assertThat(result.getType()).isEqualTo("Prompter");
+    }
+
+    @Test
+    void whenCorrectDeleteThenDeleteIsTrue() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Soloist"));
+        boolean rsl = store.delete("1");
+        assertThat(rsl).isTrue();
+    }
+
+    @Test
+    void whenIncorrectDeleteThenDeleteIsFalse() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Soloist"));
+        boolean rsl = store.delete("2");
+        assertThat(rsl).isFalse();
+    }
+
+
+    @Test
+    void whenFindByIDIsCorrect() {
         RoleStore store = new RoleStore();
         store.add(new Role("1", "Soloist"));
         Role result = store.findById("1");
@@ -22,30 +100,4 @@ class RoleStoreTest {
         assertThat(result).isNull();
     }
 
-    @Test
-    void whenAddPresentThenSameRole() {
-        RoleStore store = new RoleStore();
-        store.add(new Role("1", "Soloist"));
-        store.add(new Role("1", "Assistant"));
-        Role result = store.findById("1");
-        assertThat(result.getType()).isEqualTo("Soloist");
-    }
-
-    @Test
-    void whenRightReplace() {
-        RoleStore store = new RoleStore();
-        store.add(new Role("1", "Soloist"));
-        store.replace("1", new Role("1", "Assistant"));
-        Role result = store.findById("1");
-        assertThat(result.getType()).isEqualTo("Assistant");
-    }
-
-    @Test
-    void whenWrongReplace() {
-        RoleStore store = new RoleStore();
-        store.add(new Role("1", "Soloist"));
-        store.replace("1", new Role("10", "Assistant"));
-        Role result = store.findById("1");
-        assertThat(result.getType()).isEqualTo("Soloist");
-    }
 }
