aa890e348d128639014f45a108f97d203bad09bc
Евгений <eugene.lysakov@gmail.com>
2023-04-12 16:04:08 +0500

    SimpleQueue

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,16 +1,33 @@
 package ru.job4j.collection;
 
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    private int sizeIn;
-    private int sizeOut;
+
+    private int size;
+    private int start;
     /* добавьте переменные, если они требуются */
 
     public T poll() {
-        return null;
+        if (start == size) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        SimpleStack<T> change;
+        change = in;
+        for (int i = start; i < size; i++) {
+            out.push(change.pop());
+        }
+        T rsl = out.pop();
+        start++;
+        return rsl;
+
     }
 
     public void push(T value) {
+        in.push(value);
+        size++;
     }
 }
