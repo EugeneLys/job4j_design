ddd3de0789b81e823750fc1a2c902046be872e12
EugeneLys <eugene.lysakov@gmail.com>
2023-04-16 23:14:45 +0500

    3. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/collection/Study.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.function.BinaryOperator;
+
+public class Study {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 16;
+        System.out.println(Integer.toBinaryString(1 ^ 2));
+        System.out.println(1 | 16);
+        System.out.println(1);
+    }
+}
