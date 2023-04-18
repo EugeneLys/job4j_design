aae22d58899719a9656aa38032c435900eb0ef89
Евгений <eugene.lysakov@gmail.com>
2023-04-18 15:49:08 +0500

    10. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,71 @@
+package ru.job4j.map;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class SimpleMap<K, V> implements Map<K, V> {
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int capacity = 8;
+
+    private int count = 0;
+
+    private int modCount = 0;
+
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    @Override
+    public boolean put(K key, V value) {
+        boolean rsl = false;
+        int h = hash(Objects.hashCode(key));
+        int index = indexFor(h);
+        if (table[index] == null) {
+            table[index] = new MapEntry<K, V>(key, value);
+            count++;
+            return rsl;
+        }
+        table[count] = new MapEntry<K, V>(key, value);
+        count++;
+        return false;
+    }
+
+    private int hash(int hashCode) {
+        return hashCode ^ (hashCode>>>8);
+    }
+
+    private int indexFor(int hash) {
+        return hash & capacity - 1;
+    }
+
+    private void expand() {
+        SimpleMap<K, V> map = new SimpleMap<>();
+        map.capacity = capacity * 2;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        return false;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+
+    private static class MapEntry<K, V> {
+
+        K key;
+        V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
