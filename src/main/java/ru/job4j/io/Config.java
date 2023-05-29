c7ba452f4678d72b60712872014ca7e255460339
Евгений <eugene.lysakov@gmail.com>
2023-05-29 16:05:43 +0500

    6. Читаем файл конфигурации [#858] решаю

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        String div = "=";
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            reader.lines()
+                    .forEach(l -> values.put(l.substring(0, l.lastIndexOf(div)), l.substring(l.lastIndexOf(div) + 1)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+        //throw new UnsupportedOperationException("Don't impl this method yet!");
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+    }
+}
