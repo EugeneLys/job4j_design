fdf23bc5c11ed8d4d8bf745f769d63570569dc1c
Евгений <eugene.lysakov@gmail.com>
2023-06-01 16:54:55 +0500

    Analysis

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,20 +6,25 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
         BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+            StringBuilder builder = new StringBuilder();
             String str = reader.readLine();
             while (str != null) {
                 if (str.startsWith("400") || str.startsWith("500")) {
-                    writer.write(str + ";", 4, str.length() - 4);
-                    writer.write(";");
+                    builder.append(str, 4, str.length());
+                    builder.append(";");
+                    /*writer.write(";");*/
                     while (str.startsWith("400") || str.startsWith("500")) {
                         str = reader.readLine();
                     }
-                    writer.write(str, 4, str.length() - 4);
-                    writer.write(";");
-                    writer.newLine();
+                    builder.append(str, 4, str.length());
+                    builder.append(";");
+                    builder.append("\n");
+                   /* writer.write(";");*/
+                    /*writer.newLine();*/
                 }
                 str = reader.readLine();
             }
+            writer.write(builder.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
