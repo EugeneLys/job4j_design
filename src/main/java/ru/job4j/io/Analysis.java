094d89db94e16abcc20c7241d6847c8a757879b6
Евгений <eugene.lysakov@gmail.com>
2023-06-02 17:15:47 +0500

    7. Анализ доступности сервера [#859 #419915]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,32 +6,24 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
         BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
-            StringBuilder builder = new StringBuilder();
             String str;
             while (reader.ready()) {
                 str = reader.readLine();
                 if (str.startsWith("400") || str.startsWith("500")) {
-                    builder.append(str, 4, str.length());
-                    builder.append(";");
-                    while (str.startsWith("400") || str.startsWith("500")) {
-                        if (reader.ready()) {
-                            str = reader.readLine();
-                        } else {
-                            break;
-                        }
+                    writer.append(str, 4, str.length()).append(";");
+                    while (str.startsWith("400") || str.startsWith("500") && reader.ready()) {
+                        str = reader.readLine();
                     }
                     if (str.startsWith("200") || str.startsWith("300")) {
-                        builder.append(str, 4, str.length());
-                        builder.append(";");
-                        builder.append("\n");
+                        writer.append(str, 4, str.length()).append(";").append(System.lineSeparator());
                     }
                 }
             }
-            writer.write(builder.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static void main(String[] args) {
         Analysis analysis = new Analysis();
         analysis.unavailable("data/server.log", "data/target.csv");
