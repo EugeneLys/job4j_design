33bf70e81b472ed6344a7d2016c363c2d9043d6d
Евгений <eugene.lysakov@gmail.com>
2023-06-09 14:44:28 +0500

    10. Сканирование файловой системы [#106929]

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -1,17 +1,16 @@
 package ru.job4j.io;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class SearchFiles extends SimpleFileVisitor<Path> {
+public class SearchFiles implements FileVisitor<Path> {
 
     Predicate<Path> condition;
+    List<Path> foundFiles = new ArrayList<>();
 
     public SearchFiles(Predicate<Path> condition) {
         this.condition = condition;
@@ -19,15 +18,27 @@ public class SearchFiles extends SimpleFileVisitor<Path> {
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-
-        List<String> lines = Files.readAllLines(file);
-        for (String s: lines) {
-            if (s.contains("This is the file we need")) {
-                System.out.println("Нужный файл обнаружен!");
-                System.out.println(file.toAbsolutePath());
-                break;
-            }
+        if (condition.test(file)) {
+            foundFiles.add(file);
         }
         return FileVisitResult.CONTINUE;
+    }
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return foundFiles;
     }
 }
