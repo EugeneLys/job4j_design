c200091007d5f5705f5967ff12d93d4e5a0365d4
Евгений <eugene.lysakov@gmail.com>
2023-06-13 15:44:35 +0500

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,13 +10,32 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (!check(args)) {
+            throw new IllegalArgumentException();
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static boolean check(String[] args) {
+        boolean rsl = true;
+        File file = new File(args[0]);
+        if (args.length != 2) {
+            rsl = false;
+            System.out.println("Requires 2 arguments");
+        } else if (!file.isDirectory()) {
+            rsl = false;
+            System.out.println("Bad root argument, no such directory");
+        } else if (!args[1].equals(".js")) {
+            rsl = false;
+            System.out.println("Bad file extension argument");
+        }
+        return rsl;
     }
 }
