fd0b7e825ac1d3b4febdf2ae32d1bc6dd05d25e1
Евгений <eugene.lysakov@gmail.com>
2023-06-21 15:29:11 +0500

    11. Поиск дубликатов [#315066 #422749]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,29 +5,40 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private HashMap<FileProperty, Path> map = new HashMap<>();
-    private HashSet<String> set = new HashSet<>();
+    private HashMap<FileProperty, List<Path>> map = new HashMap<>();
+    private HashSet<FileProperty> set = new HashSet<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (file.toFile().isFile()) {
             FileProperty temp = new FileProperty(file.toFile().length(), file.toFile().getName());
+            List<Path> list = new ArrayList<>();
             if (map.containsKey(temp)) {
-                if (set.add(temp.getName())) {
-                    System.out.println(temp.getName() + ", " + temp.getSize());
-                    System.out.println(map.get(temp));
-                }
-                System.out.println(temp.getName() + ", " + temp.getSize());
-                System.out.println(file.toAbsolutePath());
+                set.add(temp);
+                list = map.get(temp);
+                list.add(file.toAbsolutePath());
+                map.replace(temp, list);
             } else {
-                map.put(temp, file.toAbsolutePath());
+                list.add(file.toAbsolutePath());
+                map.put(temp, list);
             }
         }
         return super.visitFile(file, attrs);
+    }
+
+    public void print() {
+        for (FileProperty fp : set) {
+            System.out.println(fp.getName() + ", " + fp.getSize());
+            for (Path path : map.get(fp)) {
+                System.out.println(path);
+            }
+        }
     }
 }
