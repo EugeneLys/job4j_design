658b3b6d6009f043095bef0564d87f153c0b508f
EugeneLys <eugene.lysakov@gmail.com>
2023-06-25 16:42:35 +0500

    14. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -9,7 +9,16 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     public void packFiles(List<Path> sources, File target) {
-
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toString()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void packSingleFile(File source, File target) {
@@ -23,9 +32,17 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Not enough arguments.");
+        }
         ArgsName argsName = ArgsName.of(args);
+        if (!Path.of(argsName.get("d")).toFile().isDirectory()) {
+            throw new IllegalArgumentException("Can't find directory to archive");
+        }
+        List<Path> source = Search.search(Path.of(argsName.get("d")), p -> !p.toFile().getName().endsWith(argsName.get("e")));
         Zip zip = new Zip();
+        zip.packFiles(source, new File(argsName.get("o")));
         zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
