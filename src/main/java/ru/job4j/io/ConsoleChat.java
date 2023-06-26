44308d964f270ea4123d075900f59bdda4ec8d43
EugeneLys <eugene.lysakov@gmail.com>
2023-06-26 23:31:55 +0500

    ConsoleChat

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,11 +1,10 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ConsoleChat {
@@ -21,12 +20,25 @@ public class ConsoleChat {
     }
 
     public void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); BufferedWriter bw = new BufferedWriter((Writer) readPhrases())) {
+            if (br.readLine().equals(OUT)) {
 
-    }
+            }
+            if (br.readLine().equals(STOP)) {
+                br.close();
+            }
+            if (br.readLine().equals(CONTINUE)) {
+                br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
 
     private List<String> readPhrases() {
         List<String> result = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("./src/data/text.txt", Charset.forName("WINDOWS-1251")))) {
+        try (BufferedReader br = new BufferedReader
+                (new FileReader(botAnswers, Charset.forName("WINDOWS-1251")))) {
             result = br.lines().toList();
         } catch (IOException e) {
             e.printStackTrace();
@@ -35,11 +47,15 @@ public class ConsoleChat {
     }
 
     private void saveLog(List<String> log) {
-
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+            log.forEach(pw::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        ConsoleChat cc = new ConsoleChat("", "");
+        ConsoleChat cc = new ConsoleChat("./src/data/log.txt", "./src/data/botAnswers.txt");
         cc.run();
     }
 }
