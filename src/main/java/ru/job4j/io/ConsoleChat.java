3fa07c41f3c1c138fc9ce815510128fc314e1b22
Евгений <eugene.lysakov@gmail.com>
2023-06-27 19:29:38 +0500

    16. Кодировка [#862]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -5,7 +5,9 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ConsoleChat {
     private static final String OUT = "закончить";
@@ -20,26 +22,37 @@ public class ConsoleChat {
     }
 
     public void run() {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); BufferedWriter bw = new BufferedWriter((Writer) readPhrases())) {
-            if (br.readLine().equals(OUT)) {
-
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+            List<String> list = readPhrases();
+            System.out.println("Введите сообщение: ");
+            String str = br.readLine();
+            pw.println(str);
+            while (!str.equals(OUT)) {
+                if (str.equals(STOP)) {
+                    while (!str.equals(CONTINUE)) {
+                        str = br.readLine();
+                        pw.println(str);
+                    }
+                } else {
+                    pw.println(list.get((int) (Math.random() * list.size())));
+                    System.out.println(list.get((int) (Math.random() * list.size())));
+                    str = br.readLine();
+                    pw.println(str);
+                }
             }
-            if (br.readLine().equals(STOP)) {
-                br.close();
-            }
-            if (br.readLine().equals(CONTINUE)) {
-                br.readLine();
-            }
+            br.close();
+            System.out.println("Program execution finished");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        }
+    }
 
     private List<String> readPhrases() {
         List<String> result = new ArrayList<>();
         try (BufferedReader br = new BufferedReader
                 (new FileReader(botAnswers, Charset.forName("WINDOWS-1251")))) {
-            result = br.lines().toList();
+            br.lines().forEach(result::add);
         } catch (IOException e) {
             e.printStackTrace();
         }
