262e68ca77ef1f322f1b698bbbcc9ad65be96eb5
Евгений <eugene.lysakov@gmail.com>
2023-06-28 16:07:19 +0500

    16. Кодировка [#862]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -15,6 +15,7 @@ public class ConsoleChat {
     private static final String CONTINUE = "продолжить";
     private final String path;
     private final String botAnswers;
+    private List<String> log;
 
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
@@ -22,27 +23,29 @@ public class ConsoleChat {
     }
 
     public void run() {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             List<String> list = readPhrases();
+            List<String> log = new ArrayList<>();
             System.out.println("Введите сообщение: ");
             String str = br.readLine();
-            pw.println(str);
+            log.add(str);
             while (!str.equals(OUT)) {
                 if (str.equals(STOP)) {
                     while (!str.equals(CONTINUE)) {
                         str = br.readLine();
-                        pw.println(str);
+                        log.add(str);
                     }
                 } else {
-                    pw.println(list.get((int) (Math.random() * list.size())));
-                    System.out.println(list.get((int) (Math.random() * list.size())));
+                    String answer = list.get((int) (Math.random() * list.size()));
+                    System.out.println(answer);
+                    log.add(answer);
                     str = br.readLine();
-                    pw.println(str);
+                    log.add(str);
                 }
             }
             br.close();
             System.out.println("Program execution finished");
+            saveLog(log);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,8 +53,7 @@ public class ConsoleChat {
 
     private List<String> readPhrases() {
         List<String> result = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader
-                (new FileReader(botAnswers, Charset.forName("WINDOWS-1251")))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(botAnswers, Charset.forName("WINDOWS-1251")))) {
             br.lines().forEach(result::add);
         } catch (IOException e) {
             e.printStackTrace();
