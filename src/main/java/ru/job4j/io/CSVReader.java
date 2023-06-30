78dd88f04da87a4b438c31171b75b685dcedd41b
Евгений <eugene.lysakov@gmail.com>
2023-06-30 14:03:04 +0500

    scanner

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,9 +10,12 @@ import java.util.StringJoiner;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
+            StringJoiner stringJoiner = new StringJoiner(",");
+            Scanner scanner = new Scanner(argsName.get("path"));
+            stringJoiner.add(scanner.next());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         /* здесь добавьте валидацию принятых параметров*/
         ArgsName argsName = ArgsName.of(args);
         handle(argsName);
