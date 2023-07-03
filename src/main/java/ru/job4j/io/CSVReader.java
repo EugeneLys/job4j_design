808ffc0597ea2b6e5151bc41a172e95b496de906
Евгений <eugene.lysakov@gmail.com>
2023-07-03 18:17:49 +0500

    scanner

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -5,14 +5,33 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
-            StringJoiner stringJoiner = new StringJoiner(",");
-            Scanner scanner = new Scanner(argsName.get("path"));
-            stringJoiner.add(scanner.next());
+        StringJoiner stringJoiner = new StringJoiner(",");
+        Scanner scanner = new Scanner(argsName.get("path"));
+        String[] filters = argsName.get("filter").split(";");
+        String[] words = scanner.nextLine().split(",");
+        List<Integer> indexes = new ArrayList<>();
+        for (String filter : filters) {
+            for (int j = 0; j < words.length; j++) {
+                if (filter.equals(words[j])) {
+                    indexes.add(j);
+                }
+            }
+        }
+        String[] result;
+        System.out.println();
+        while (scanner.hasNextLine()) {
+            words = scanner.nextLine().split(";");
+            for (int i : indexes) {
+                stringJoiner.add(words[i]);
+            }
+            System.out.println(stringJoiner);
+        }
     }
 
     public static void main(String[] args) throws Exception {
