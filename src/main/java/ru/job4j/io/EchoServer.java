02be4aae93593a85e1f846317d66368ca16becb2
Евгений <eugene.lysakov@gmail.com>
2023-07-06 15:57:47 +0500

    EchoServer

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,16 +13,13 @@ public class EchoServer {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.regionMatches(str.indexOf('=') + 1, "Bye", 0, 3)) {
-                            System.out.println("Server is closed");
-                            server.close();
-                        }
-                        System.out.println(str.toUpperCase());
+                    String str = in.readLine();
+                    if (str.contains("msg=Bye")) {
+                        System.out.println("Server is closed");
+                        server.close();
                     }
-                    out.flush();
                 }
             }
         }
