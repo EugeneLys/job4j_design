bcefb60af1ec836240b44cd0deb4529ffba659af
EugeneLys <eugene.lysakov@gmail.com>
2023-08-21 23:27:45 +0500

    2. Группировка и агрегатные функции [#373618]

--- db/devices.sql
+++ db/devices.sql
@@ -1,8 +1,7 @@
 create table devices(
     id serial primary key,
     name varchar(255),
-    price float
-);
+    price float);
 
 create table people(
     id serial primary key,
@@ -17,8 +16,19 @@ create table devices_people(
 
 insert into devices(name, price) values ('phone', 15000), ('TV', 30000), ('notebook', 50000);
 insert into people(name) values ('Ivan'), ('Petr'), ('Anna');
-insert into devices_people(device_id, people_id) values (7, 10), (7, 11), (7, 12);
-insert into devices_people(device_id, people_id) values (8, 10), (8, 11);
-insert into devices_people(device_id, people_id) values (9, 12);
+insert into devices_people(device_id, people_id) values (1, 1), (1, 2), (1, 3);
+insert into devices_people(device_id, people_id) values (2, 1), (2, 2);
+insert into devices_people(device_id, people_id) values (3, 1);
+
 select avg(price) from devices;
 
+select people_id, avg(price) from devices 
+join devices_people
+on device_id=devices.id
+group by people_id;
+
+select people_id, avg(price) from devices 
+join devices_people
+on device_id=devices.id
+group by people_id
+having avg(price) > 5000;
