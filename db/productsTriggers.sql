583f28b280d18ff48d83a82dd00ca66ce7a72073
Евгений <eugene.lysakov@gmail.com>
2023-08-31 13:09:43 +0500

    2. Триггеры [#504803]

--- db/productsTriggers.sql
+++ db/productsTriggers.sql
@@ -42,6 +42,8 @@ create trigger tax_trigger
     for each statement
     execute procedure tax();
 
+
+
 create or replace function impost()
     returns trigger as
 $$
@@ -59,3 +61,49 @@ create trigger impost_trigger
     referencing new table as inserted
     for each statement
     execute procedure impost();
+
+
+
+create or replace function bef_impost()
+	returns trigger as
+$$
+	BEGIN
+		update products
+		set price = price + price*0.2
+		where id = new.id;
+		return NEW;
+	END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger bef_impost_trigger
+    before insert 
+	on products
+    for each row
+    execute procedure bef_impost();
+
+
+
+create table history_of_price (
+    id serial primary key,
+    name varchar(50),
+    price integer,
+    date timestamp
+);
+
+create or replace function fix_price()
+    returns trigger as
+$$
+    BEGIN
+        INSERT INTO history_of_price(name, price, date)
+		VALUES (new.name, new.price, CURRENT_DATE);
+		return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger write_history_trigger
+    after insert
+    on products
+    for each row
+    execute procedure fix_price();
