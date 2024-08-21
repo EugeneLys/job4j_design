9dae7b6aee25d7c8fdc87bd87bfd815fabea2d70
Евгений <eugene.lysakov@gmail.com>
2024-08-21 15:59:21 +0500

    Generator

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
+    public class GeneratorTest {
+
+        @Test
+        public void whenCorrectGeneration() {
+            Generator gen = new TemplateGenerator();
+            String template = "${city} is the capital of ${country}?";
+            String check = "Moscow is the capital of Russia";
+            Map<String, String> map = new HashMap<>();
+            map.put("city", "Moscow");
+            map.put("country", "Russia");
+            Map<String, String> checkMap = null;
+            Assertions.assertEquals(gen.produce(template, map), check);
+        }
+
+        @Test
+        public void whenNoSuchKey() {
+            Generator gen = new TemplateGenerator();
+            String template = "${city} is the capital of ${country}?";
+            Map<String, String> map = new HashMap<>();
+            map.put("city", "Moscow");
+            map.put("country", "Russia");
+            Map<String, String> checkMap = null;
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> gen.produce(template, map))
+                    .withMessageContaining("no key found");
+        }
+
+    @Test
+    public void whenExtraKeyFound() {
+        Generator gen = new TemplateGenerator();
+        String template = "${city} is the capital of ${country}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("city", "Moscow");
+        map.put("country", "Russia");
+        Map<String, String> checkMap = null;
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> gen.produce(template, map))
+                .withMessageContaining("extra key found");
+    }
+}
