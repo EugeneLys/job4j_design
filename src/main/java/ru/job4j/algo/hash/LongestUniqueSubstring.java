9f30bb4df99446d6a30455ba5930132043f07993
EugeneLys <eugene.lysakov@gmail.com>
2024-12-17 23:42:57 +0500

    LongestUniqueSubstring

---
+++ src/main/java/ru/job4j/algo/hash/LongestUniqueSubstring.java
@@ -0,0 +1,9 @@
+package ru.job4j.algo.hash;
+
+import java.util.HashMap;
+
+public class LongestUniqueSubstring {
+    public static String longestUniqueSubstring(String str) {
+        return str;
+    }
+}
