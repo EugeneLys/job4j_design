e96d3462d58332e8c8eeb999a61dee49a3537fdd
EugeneLys <eugene.lysakov@gmail.com>
2024-12-22 00:09:05 +0500

    LongestUniqueSubstring

--- src/main/java/ru/job4j/algo/hash/LongestUniqueSubstring.java
+++ src/main/java/ru/job4j/algo/hash/LongestUniqueSubstring.java
@@ -3,7 +3,32 @@ package ru.job4j.algo.hash;
 import java.util.HashMap;
 
 public class LongestUniqueSubstring {
+    /*
+     * Метод использует объект HashMap
+     * для быстрого (O(1)) поиска дубликата в последовательности символов строки
+     */
     public static String longestUniqueSubstring(String str) {
-        return str;
+        var result = "";
+        char[] array = str.toCharArray();
+        var map = new HashMap<Character, Byte>();
+        Byte b = 0;
+        int cursor = 0, length = 0, max = 0;
+        for (int i = 0; i < array.length; i++) {
+            while (!map.containsKey(array[i])) {
+                map.put(array[i], b);
+                length++;
+                if (i < array.length - 1) {
+                    i++;
+                }
+            }
+            if (length > max) {
+                result = str.substring(cursor, cursor + length);
+                max = length;
+            }
+            i = cursor++;
+            length = 0;
+            map.clear();
+            }
+        return result;
     }
 }
