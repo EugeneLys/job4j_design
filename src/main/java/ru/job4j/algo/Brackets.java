c91f7fb1f98c6b9f7c756796f092752d3ca49f3e
EugeneLys <eugene.lysakov@gmail.com>
2025-02-01 11:55:36 +0500

    Brackets

---
+++ src/main/java/ru/job4j/algo/Brackets.java
@@ -0,0 +1,35 @@
+package ru.job4j.algo;
+
+import java.util.Stack;
+
+public class Brackets {
+    public boolean isValid(String s) {
+        var result = true;
+        var elements = s.split("");
+        var stack = new Stack<>();
+        for (var element : elements) {
+            if (element.equals("{") || element.equals("[") || element.equals("(")) {
+                stack.push(element);
+            }
+            if (element.equals(")")) {
+                if (stack.isEmpty() || !stack.pop().equals("(")) {
+                    result = false;
+                }
+            }
+            if (element.equals("}")) {
+                if (stack.isEmpty() || !stack.pop().equals("{")) {
+                    result = false;
+                }
+            }
+            if (element.equals("]")) {
+                if (stack.isEmpty() || !stack.pop().equals("[")) {
+                    result = false;
+                }
+            }
+        }
+        if (!stack.isEmpty()) {
+            result = false;
+        }
+        return result;
+    }
+}
