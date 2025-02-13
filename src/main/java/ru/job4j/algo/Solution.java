812449528649b76b42f3a117df87f54a95a28a7a
EugeneLys <eugene.lysakov@gmail.com>
2025-02-14 00:45:49 +0500

    GasStation

--- src/main/java/ru/job4j/algo/Solution.java
+++ src/main/java/ru/job4j/algo/Solution.java
@@ -3,33 +3,26 @@ package ru.job4j.algo;
 import java.util.*;
 
 class Solution {
-    public double[] medianSlidingWindow(int[] nums, int k) {
-        double[] result = new double[nums.length - k + 1];
-        int next = 0, count = 0;
-        double temp = 0;
-        int treeCount = 0;
-        double res = 0;
-        TreeMap<Integer, Integer> tree = new TreeMap<>();
-        for (int i = 0; i < nums.length - k + 1; i++) {
-            while (count < k) {
-                /*if (tree.containsKey(nums[i + count])) {
-                    treeCount++;
-                }*/
-                tree.put(nums[i + count], nums[i + count]);
-                count++;
-            }
-            count = k - tree.size() % 2 == 0 ? tree.size() / 2 : (tree.size() + 1) / 2;
-            //count = (k - treeCount) % 2 == 0 && k % 2 == 0  ? ((k - treeCount) / 2 - 1) : ((k - treeCount - 1) / 2);
-            while (count > 0) {
-                temp = (double) tree.pollFirstEntry().getValue();
-                count--;
-            }
-            res = k % 2 == 0 && tree.size() > 1 ? (temp + (double) tree.pollFirstEntry().getValue()) / 2 : temp;
-            result[next++] = res;
-            tree.clear();
-            count = 0;
-            //treeCount = 0;
+    public int maxProfit(int[] prices) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        int end;
+        for (int i = 0; i < prices.length; i++) {
+            map.put(i, prices[i]);
         }
-        return result;
+        for (int i = 0; i < map.size() - 1; i++) {
+            end = i + 1;
+            while (end < map.size()) {
+                if (map.get(end) > map.get(i) && map.get(end) - map.get(i) > max) {
+                    max = map.get(end) - map.get(i);
+                }
+                if (end < map.size()) {
+                    end++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return max;
     }
 }
--- src/test/java/ru/job4j/algo/SolutionTest.java
+++ src/test/java/ru/job4j/algo/SolutionTest.java
@@ -9,36 +9,8 @@ class SolutionTest {
     @Test
     void whenCorrect() {
         Solution solution = new Solution();
-        int[] nums = new int[] {1, 3, -1, -3, 5, 3, 6, 7};
-        int k = 3;
-        double[] result = solution.medianSlidingWindow(nums, k);
-        assertThat(new double[] {1.00000, -1.00000, -1.00000, 3.00000, 5.00000, 6.00000}).containsExactly(result);
-    }
-
-    @Test
-    void whenCorrect2() {
-        Solution solution = new Solution();
-        int[] nums = new int[] {2147483647, 2147483647};
-        int k = 2;
-        double[] result = solution.medianSlidingWindow(nums, k);
-        assertThat(new double[] {2147483647}).containsExactly(result);
-    }
-
-    @Test
-    void whenCorrect3() {
-        Solution solution = new Solution();
-        int[] nums = new int[] {5, 2, 2, 7, 3, 7, 9, 0, 2, 3};
-        int k = 9;
-        double[] result = solution.medianSlidingWindow(nums, k);
-        assertThat(new double[] {3.00000, 3.00000}).containsExactly(result);
-    }
-
-    @Test
-    void whenCorrect4() {
-        Solution solution = new Solution();
-        int[] nums = new int[] {1, 4, 2, 3};
-        int k = 4;
-        double[] result = solution.medianSlidingWindow(nums, k);
-        assertThat(new double[] {2.50000}).containsExactly(result);
+        int[] nums = new int[] {1, 2};
+        int result = solution.maxProfit(nums);
+        assertThat(1).isEqualTo(result);
     }
 }
