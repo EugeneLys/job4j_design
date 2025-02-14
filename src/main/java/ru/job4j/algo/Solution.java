01998f9a2961096c520263fbd41e5a62d68dffb0
Евгений <eugene.lysakov@gmail.com>
2025-02-14 16:44:30 +0500

    Solution

--- src/main/java/ru/job4j/algo/Solution.java
+++ src/main/java/ru/job4j/algo/Solution.java
@@ -3,26 +3,30 @@ package ru.job4j.algo;
 import java.util.*;
 
 class Solution {
-    public int maxProfit(int[] prices) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int max = 0;
-        int end;
-        for (int i = 0; i < prices.length; i++) {
-            map.put(i, prices[i]);
-        }
-        for (int i = 0; i < map.size() - 1; i++) {
-            end = i + 1;
-            while (end < map.size()) {
-                if (map.get(end) > map.get(i) && map.get(end) - map.get(i) > max) {
-                    max = map.get(end) - map.get(i);
-                }
-                if (end < map.size()) {
-                    end++;
-                } else {
-                    break;
-                }
+    public int maxProfit(int[] prices, int fee) {
+        int total = 0;
+        int tempIndex;
+        int currIndex = 0;
+        int temp;
+        int tempProfit = 0;
+        int current = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            while (i < prices.length - 1 && prices[i] <= current + fee) {
+                i++;
+            }
+            temp = prices[i];
+            tempIndex = i;
+            while (tempIndex < prices.length - 1 && prices[tempIndex] - fee < temp) {
+                tempIndex++;
+            }
+            if (prices[tempIndex] - fee >= temp) {
+                temp = prices[tempIndex];
+            }
+            total += temp - current - fee;
+            if (i < prices.length - 1) {
+                current = prices[i + 1];
             }
         }
-        return max;
+        return total;
     }
 }
--- src/test/java/ru/job4j/algo/SolutionTest.java
+++ src/test/java/ru/job4j/algo/SolutionTest.java
@@ -9,8 +9,18 @@ class SolutionTest {
     @Test
     void whenCorrect() {
         Solution solution = new Solution();
-        int[] nums = new int[] {1, 2};
-        int result = solution.maxProfit(nums);
-        assertThat(1).isEqualTo(result);
+        int[] nums = new int[] {1,3,2,8,4,9};
+        int fee = 2;
+        int result = solution.maxProfit(nums, fee);
+        assertThat(8).isEqualTo(result);
+    }
+
+    @Test
+    void whenCorrect2() {
+        Solution solution = new Solution();
+        int[] nums = new int[] {1,3,7,5,10,3};
+        int fee = 3;
+        int result = solution.maxProfit(nums, fee);
+        assertThat(6).isEqualTo(result);
     }
 }
