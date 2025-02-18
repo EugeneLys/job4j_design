bd4d7b8aaaeba0112be443a483b9be2cc343b415
Евгений <eugene.lysakov@gmail.com>
2025-02-18 11:48:14 +0500

    TreeUtils

--- src/main/java/ru/job4j/newcoll/tree/TreeUtils.java
+++ src/main/java/ru/job4j/newcoll/tree/TreeUtils.java
@@ -2,8 +2,7 @@ package ru.job4j.newcoll.tree;
 
 import ru.job4j.collection.SimpleQueue;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.ArrayList;
 
 public class TreeUtils<T> {
 
@@ -41,8 +40,24 @@ public class TreeUtils<T> {
      * @throws IllegalArgumentException если root является null
      */
     public Iterable<T> findAll(Node<T> root) {
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
         SimpleQueue<Node<T>> queue = new SimpleQueue<>();
-        /*TODO реализуйте метод.*/
-        return (Iterable<T>) queue;
+        queue.push(root);
+        ArrayList<T> list = new ArrayList<>();
+        list.add(root.getValue());
+        Node<T> node = queue.poll();
+        while (node != null) {
+            if (node.getChildren().isEmpty()) {
+                break;
+            }
+            for (Node<T> n : node.getChildren()) {
+                queue.push(n);
+                list.add(n.getValue());
+            }
+            node = queue.poll();
+        }
+        return list;
     }
 }
