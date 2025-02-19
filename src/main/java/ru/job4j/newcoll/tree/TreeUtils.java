6a0bd5f6feaedacc115d18623e87cb39e905798a
EugeneLys <eugene.lysakov@gmail.com>
2025-02-19 23:40:15 +0500

    TreeUtils

--- src/main/java/ru/job4j/newcoll/tree/TreeUtils.java
+++ src/main/java/ru/job4j/newcoll/tree/TreeUtils.java
@@ -1,8 +1,10 @@
 package ru.job4j.newcoll.tree;
 
 import ru.job4j.collection.SimpleQueue;
+import ru.job4j.collection.SimpleStack;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class TreeUtils<T> {
 
@@ -59,5 +61,46 @@ public class TreeUtils<T> {
             node = queue.poll();
         }
         return list;
+    }
+
+    /**
+     * Метод обходит дерево root и добавляет к узлу с ключом parent
+     * новый узел с ключом child, при этом на момент добавления узел с ключом parent
+     * уже должен существовать в дереве, а узла с ключом child в дереве быть не должно
+     * @param root корень дерева
+     * @param parent ключ узла-родителя
+     * @param child ключ узла-потомка
+     * @return true если добавление произошло успешно и false в обратном случае.
+     * @throws IllegalArgumentException если root является null
+     */
+    public boolean add(Node<T> root, T parent, T child) {
+        /* TODO реализуйте метод */
+        return false;
+    }
+
+    /**
+     * Метод обходит дерево root и возвращает первый найденный узел с ключом key
+     * @param root корень дерева
+     * @param key ключ поиска
+     * @return узел с ключом key, завернутый в объект типа Optional
+     * @throws IllegalArgumentException если root является null
+     */
+    public Optional<Node<T>> findByKey(Node<T> root, T key) {
+        SimpleStack<Node<T>> stack = new SimpleStack<>();
+        /* TODO реализуйте метод */
+        return null;
+    }
+
+    /**
+     * Метод обходит дерево root и возвращает первый найденный узел с ключом key,
+     * при этом из дерева root удаляется все поддерево найденного узла
+     * @param root корень дерева
+     * @param key ключ поиска
+     * @return узел с ключом key, завернутый в объект типа Optional
+     * @throws IllegalArgumentException если root является null
+     */
+    public Optional<Node<T>> divideByKey(Node<T> root, T key) {
+        /* TODO реализуйте метод */
+        return null;
     }
 }
