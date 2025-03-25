1f5de0681574cdefc31f52b5f9400a56cadf710d
Евгений <eugene.lysakov@gmail.com>
2025-03-25 17:29:40 +0500

    Solution

--- src/main/java/ru/job4j/algo/Solution.java
+++ src/main/java/ru/job4j/algo/Solution.java
@@ -4,24 +4,28 @@ import java.util.*;
 
 class Solution {
 
-    Node root;
-
     public int numIslands(char[][] grid) {
-        if (root == null) {
-            root = makeNode(0, 0);
+        int result = 0;
+        int lenght1 = grid.length;
+        int length2 = grid[0].length;
+        for (int i = 0; i < lenght1; i++) {
+            for (int j = 0; j < length2; j++) {
+                if (grid[i][j] == '1') {
+                    result++;
+                    mark(grid, i , j);
+                }
+            }
         }
-        return 0;
+        return result;
     }
 
-    private Node makeNode(int i, int j) {
-
-    }
-
-    class Node {
-        int row;
-        int column;
-        Node left;
-        Node right;
-        Node down;
+    private void mark(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
+            return;
+        grid[i][j] = '0';
+        mark(grid, i, j+1);
+        mark(grid, i, j-1);
+        mark(grid, i+1, j);
+        mark(grid, i-1, j);
     }
 }
