7dc638deb6f20b562cf0514cd50be269f26c3dd3
EugeneLys <eugene.lysakov@gmail.com>
2025-05-02 18:52:50 +0500

    BankMaxLoadTime

--- src/main/java/ru/job4j/algo/BankMaxLoadTime.java
+++ src/main/java/ru/job4j/algo/BankMaxLoadTime.java
@@ -1,15 +1,37 @@
 package ru.job4j.algo;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class BankMaxLoadTime {
 
     public static int[] findMaxLoadTime(List visitTimes) {
-        int maxLoadStartTime = 0;
-        int maxLoadEndTime = 0;
-        return new int[]{maxLoadStartTime, maxLoadEndTime};
+        if (visitTimes.isEmpty()) {
+            return null;
+        }
+        int start = ((int[]) visitTimes.get(0))[0];
+        int end = ((int[]) visitTimes.get(0))[1];
+        int maxLoadStartTime = start;
+        int count = 0;
+        int temp = 0;
+        for (int i = 1; i < visitTimes.size(); i++) {
+            while (((int[]) visitTimes.get(i))[0] <= end) {
+                end = Math.min(end, ((int[]) visitTimes.get(i))[1]);
+                start = Math.max(start, ((int[]) visitTimes.get(i))[0]);
+                temp++;
+                if (i < visitTimes.size() - 1) {
+                    i++;
+                } else {
+                    break;
+                }
+            }
+            if (temp > count) {
+                count = temp;
+                maxLoadStartTime = start;
+            }
+            temp = 0;
+
+        }
+        return new int[]{maxLoadStartTime, maxLoadStartTime + 1};
     }
 
      static class Event implements Comparable<Event> {
@@ -28,7 +50,6 @@ public class BankMaxLoadTime {
             }
             return Integer.compare(this.time, other.time);
         }
-
     }
 
     enum EventType {
