8b724663ebff62614239b9d0ddc7512957c31d9f
Евгений <eugene.lysakov@gmail.com>
2025-05-26 13:37:40 +0500

    Brackets 02

--- src/main/java/ru/job4j/algo/Brackets.java
+++ src/main/java/ru/job4j/algo/Brackets.java
@@ -4,7 +4,6 @@ import java.util.Stack;
 
 public class Brackets {
     public boolean isValid(String s) {
-
         var result = true;
         var elements = s.split("");
         var stack = new Stack<>();
