5eb93ce02f4eeac83c4faeaeeeaf00edad7c0940
Евгений <eugene.lysakov@gmail.com>
2025-05-26 13:41:52 +0500

    Brackets 1

--- src/main/java/ru/job4j/algo/Brackets.java
+++ src/main/java/ru/job4j/algo/Brackets.java
@@ -11,6 +11,7 @@ public class Brackets {
             if (element.equals("{") || element.equals("[") || element.equals("(")) {
                 stack.push(element);
             }
+
             if (element.equals(")")) {
                 if (stack.isEmpty() || !stack.pop().equals("(")) {
                     result = false;
@@ -21,6 +22,7 @@ public class Brackets {
                     result = false;
                 }
             }
+
             if (element.equals("]")) {
                 if (stack.isEmpty() || !stack.pop().equals("[")) {
                     result = false;
