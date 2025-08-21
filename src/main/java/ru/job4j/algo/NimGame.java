0e97de6be9bc4130e7d336d60898a2c1928eae96
EugeneLys <eugene.lysakov@gmail.com>
2025-08-21 13:51:12 +0500

    NimGame

---
+++ src/main/java/ru/job4j/algo/NimGame.java
@@ -0,0 +1,10 @@
+package ru.job4j.algo;
+
+public class NimGame {
+    public boolean canWinNim(int n) {
+        if (n % 4 != 0) {
+            return false;
+        }
+        return true;
+    }
+}
