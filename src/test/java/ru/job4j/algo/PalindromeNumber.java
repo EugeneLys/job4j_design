45376a0edd8d0801ab4a4d5f487635c42f5e5265
EugeneLys <eugene.lysakov@gmail.com>
2025-08-24 12:25:00 +0500

    united ombined consolidated comment

--- src/test/java/ru/job4j/algo/PalindromeNumber.java
+++ src/test/java/ru/job4j/algo/PalindromeNumber.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PalindromeNumber {
+
+
     public boolean isPalindrome(int x) {
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
